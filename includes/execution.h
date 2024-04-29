67fac1f6cccd9b834aef3974b6749bf6f4a857ee
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2024-04-29 13:03:58 +0400

    Update execution.h

--- includes/execution.h
+++ includes/execution.h
@@ -6,7 +6,7 @@
 /*   By: emilin <emilin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 16:09:46 by emilin            #+#    #+#             */
-/*   Updated: 2024/04/11 16:14:00 by emilin           ###   ########.fr       */
+/*   Updated: 2024/04/27 16:01:10 by emilin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 
 /* Libraries from includes folder */
 # include "env.h"
+# include "parsing.h"
 # include "tokenizer.h"
 
 typedef struct s_shell
 {
-	char	*buff;
-	char	**env;
-	t_env	*env_list;
-	t_token	*token_lst;
-	// t_cmd				*cmds;
-}			t_shell;
+	char		*buff;
+	char		**env;
+	t_env		*env_list;
+	t_token		*token_lst;
+	t_tree_node	*tree;
+}				t_shell;
 
 #endif
